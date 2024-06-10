9069b6ca3cb6c0eb8dff8201d5f5b3ba4ba5b058
kohei240155 <k.pino519@gmail.com>
2024-06-10 22:48:27 +0900

    Add GlobalNav

---
+++ src/app/components/GlobalNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const GlobalNav = () => (
+    <nav className="global-nav">
+        <ul>
+            <li><a href="#">menu</a></li>
+            <li><a href="#">blog</a></li>
+            <li><a href="#">access</a></li>
+        </ul>
+    </nav>
+);
+
+export default GlobalNav;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "./components/Header";
 import MainVisual from "./components/MainVisual";
+import GlobalNav from "./components/GlobalNav";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <MainVisual />
+      <GlobalNav />
     </div>
   );
 }
