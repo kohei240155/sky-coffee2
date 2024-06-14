5dc16f0c75a199f31591b3df78bcdb8abaa9a255
kohei240155 <k.pino519@gmail.com>
2024-06-14 21:57:51 +0900

    Add Blog1

---
+++ src/app/components/Blog.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Blog = () => (
+    <section className="blog">
+        <div className="container">
+            <h2 className="heading">blog<span className="subtitle">ブログ</span></h2>
+            <div className="list">
+                <div className="item"></div>
+            </div>
+        </div>
+    </section>
+)
+
+export default Blog
