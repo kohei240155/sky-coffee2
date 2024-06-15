844ce80f7e26340f9582ad3ed62a934db314b1dd
kohei240155 <k.pino519@gmail.com>
2024-06-15 11:21:05 +0900

    Add Blog CSS

--- src/app/components/Blog.tsx
+++ src/app/components/Blog.tsx
@@ -4,9 +4,7 @@ import React from "react";
 const Blog = () => (
   <section className="blog">
     <div className="container">
-      <h2 className="heading">
-        blog<span className="subtitle">ブログ</span>
-      </h2>
+      <h2 className="heading">blog<span className="subtitle">ブログ</span></h2>
       <div className="list">
         <div className="item">
           <a href="#">
